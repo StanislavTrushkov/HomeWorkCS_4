0de61d9d42d1859ef157c2cc7e14eeef1fc2994b
stanislav <stas09091994@gmail.com>
2022-12-07 21:46:47 +0300

    The homework file is working

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+using System;
+{
+int Exp(int numA, int numB)
+    {
+            int result = 1;
+        for(int i=1; i <= numB; i++)
+        {
+            result = result * numA;
+        }
+        return result;
+    }
+
+Console.Write("Введите число A: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Exponentiation = Exp(numA, numB);
+Console.WriteLine($"Число {numA} в степени {numB} = " + Exponentiation);
+
+    Console.ReadKey();
+}
--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+using System;
+{
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int sum(int num)
+{
+
+    int counter = Convert.ToString(num).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = num - num % 10;
+        result = result + (num - advance);
+        num = num / 10;
+    }
+    return result;
+}
+
+int sumNum = sum(num);
+Console.WriteLine($"Сумма цифр числа {num}: " + sumNum);
+
+    Console.ReadKey();
+}
